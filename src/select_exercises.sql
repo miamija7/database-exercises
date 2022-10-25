a709b35f926ca5c413ff50db64a335b9d366cf6f
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-10-25 15:58:38 -0500

    mysql statements

--- src/select_exercises.sql
+++ src/select_exercises.sql
@@ -1,10 +1,8 @@
 USE codeup_test_db;
 
-SELECT * FROM albums WHERE artist = 'Pink Floyd';
-SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
-SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
-SELECT name FROM albums WHERE sales < 20.0;
-SELECT name FROM albums WHERE genre = 'Rock';
-
-
+SELECT name AS 'Pink Floyd Albums' FROM albums WHERE artist = 'Pink Floyd';
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' AS 'Info', release_date AS 'Year' FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT name AS 'Albums released in the 1990s' FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+SELECT name AS 'Albums with 20 million certified sales or less' FROM albums WHERE sales < 20.0;
+SELECT name AS '"Rock" Genre' FROM albums WHERE genre = 'Rock';
 
