b28c99899b20beb868782c5f73666794d7cf9f32
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-10-27 15:05:18 -0500

    statements

--- src/update_exercises.sql
+++ src/update_exercises.sql
@@ -1,3 +1,10 @@
 USE codeup_test_db;
 
-SELECT name AS 'All Albums' FROM albums;
+SELECT * FROM albums;
+UPDATE albums SET sales = sales * 10;
+
+SELECT name  AS 'All albums released before 1980', release_date AS 'Release Date' FROM albums WHERE release_date < 1981;
+UPDATE albums SET release_date = release_date - 100 WHERE release_date < 1981;
+
+SELECT name AS 'Albums by Michael Jackson' FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
